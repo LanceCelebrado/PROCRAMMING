81d83057d3dcdb384a432a7d2792ae4e86b41858
LanceCelebrado <105404873+LanceCelebrado@users.noreply.github.com>
2022-05-20 19:26:18 +0800

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -2,14 +2,14 @@ from tkinter import *
 import time
 
 class StopWatch(Frame):  
-    """ Implements a stop watch frame widget. """                                                                
+                                                                  
     def __init__(self, parent=None, **kw):        
         Frame.__init__(self, parent, kw)
         self._start = 0.0        
         self._elapsedtime = 0.0
         self._running = 0
         self.timestr = StringVar()
-        #self.lapstr = StringVar()
+        
         self.e = 0
         self.m = 0
         self.makeWidgets()
