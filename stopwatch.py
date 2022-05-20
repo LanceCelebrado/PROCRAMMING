e48c5b3918ad81f13c325c3f35c3262d80247408
LanceCelebrado <105404873+LanceCelebrado@users.noreply.github.com>
2022-05-20 19:26:40 +0800

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -10,9 +10,3 @@ class StopWatch(Frame):
         self._running = 0
         self.timestr = StringVar()
         
-        self.e = 0
-        self.m = 0
-        self.makeWidgets()
-        self.laps = []
-        self.lapmod2 = 0
-        self.today = time.strftime("%d %b %Y %H-%M-%S", time.localtime())
