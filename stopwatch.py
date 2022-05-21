3ced7fe47210f63d65db4e65b7e34a8ade853f8c
LanceCelebrado <105404873+LanceCelebrado@users.noreply.github.com>
2022-05-21 21:34:28 +0800

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -1,23 +1,7 @@
-    def Start(self):                                                     
-         """ Start the stopwatch, ignore if running. """
-         if not self._running:            
-            self._start = time.time() - self._elapsedtime
-            self._update()
-            self._running = 1        
+from tkinter import *
+import time
+
+class StopWatch(Frame):
     
-    def Stop(self):                                    
-        """ Stop the stopwatch, ignore if stopped. """
-        if self._running:
-            self.after_cancel(self._timer)            
-            self._elapsedtime = time.time() - self._start    
-            self._setTime(self._elapsedtime)
-            self._running = 0
-    
-    def Reset(self):                                  
-        """ Reset the stopwatch. """
-        self._start = time.time()         
-        self._elapsedtime = 0.0
-        self.laps = []   
-        self._setTime(self._elapsedtime)
 
         
