d2c9a1179b9175d8b9a1a066ca4d4eff2ea1c86d
XerdaivRelunio <105405355+XerdaivRelunio@users.noreply.github.com>
2022-05-22 00:14:38 +0800

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -22,5 +22,18 @@ class StopWatch(Frame):
         hseconds = int ((elap - minutes*60.0 - seconds)*100)
         return '%02d:%02d:%02d' % (minutes, seconds, hseconds)
                        
-
-        
+    def Lap(self):
+        '''Makes a lap, only if started'''
+        tempo = self._elapsedtime - self.lapmod2
+        if self.running:
+            self.laps.append(self._setLapTime(tempo))
+            self.m.insert(END, self.laps[-1])
+            self.m.yview_moveto(1)
+            self.lapmod2 = self._elapsedtime
+            
+    def GravaCSV(self):
+        '''Get the name of the stopwatch and create a file to store the laps'''
+        file = str(self.e.get()) + '-'
+        with open(file + self.today + '.txt', 'wb') as lapfile:
+            for lap in self.lalps
+                lapfile.write((bytes(str(lap) + '\n', 'utf-8')))
