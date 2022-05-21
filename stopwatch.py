8722c5def97673f7df6f55b23d0ff14f5699cfb1
LanceCelebrado <105404873+LanceCelebrado@users.noreply.github.com>
2022-05-21 21:48:07 +0800

    lap system with previous lap shown

--- stopwatch.py
+++ stopwatch.py
@@ -3,5 +3,24 @@ import time
 
 class StopWatch(Frame):
     
+     
+        
+    def _update(self):
+        self._elapsedtime = time.time() - self._start
+        self._setTime(self._elapsedtime)
+        self._timer = self.after(50, self._update)
+        
+    def _setTime(self, elap):
+        minutes = int(elap/60)
+        seconds = int(elap - minutes*60.0)
+        hseconds = int ((elap - minutes*60.0 - seconds)*100)
+        self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
+                         
+    def _setLapTime(self, elap):
+        minutes = int(elap/60)
+        seconds = int(elap - minutes*60.0)
+        hseconds = int ((elap - minutes*60.0 - seconds)*100)
+        return '%02d:%02d:%02d' % (minutes, seconds, hseconds)
+                       
 
         
