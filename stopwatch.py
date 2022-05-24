63b1c219a24baebdb9e3a9545588ebc31fb188f8
RazziaPatun-og <105410546+RazziaPatun-og@users.noreply.github.com>
2022-05-24 22:55:07 +0800

    added start-reset

--- stopwatch.py
+++ stopwatch.py
@@ -54,6 +54,25 @@ class StopWatch(Frame):
         seconds = int(elap - minutes*60.0)
         hseconds = int ((elap - minutes*60.0 - seconds)*100)
         return '%02d:%02d:%02d' % (minutes, seconds, hseconds)
+    
+        def Start(self):                                                     
+        if not self._running:            
+            self._start = time.time() - self._elapsedtime
+            self._update()
+            self._running = 1        
+    
+    def Stop(self):                                    
+        if self._running:
+            self.after_cancel(self._timer)            
+            self._elapsedtime = time.time() - self._start    
+            self._setTime(self._elapsedtime)
+            self._running = 0
+    
+    def Reset(self):                                  
+        self._start = time.time()         
+        self._elapsedtime = 0.0
+        self.laps = []   
+        self._setTime(self._elapsedtime)
                        
     def Lap(self):
         
